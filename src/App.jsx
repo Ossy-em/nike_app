d728e1e3a614274e70ceec875a4659fea7355c23
Ossy-em <emosinachi@gmail.com>
2024-05-10 00:30:56 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-// import { Nav } from "./components";
+import Nav from "./components/Nav"
 import {
   CustomerReviews,
   Footer,
@@ -12,7 +12,7 @@ import {
 } from "./sections";
 const App = () => (
 <main className='relative'>
-      {/* <Nav/> */}
+      <Nav/>
       <section className='xl:padding-1 wide:padding-r padding-b'>
         <Hero/>
       </section>
